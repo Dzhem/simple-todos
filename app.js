af499262e5774a028028b1c9dcd276f401a61adc
detistalingrada <zhemchuzhnov.d@gmail.com>
2022-11-01 12:35:19 +0300

    Функционал добавления todo в список

---
+++ app.js
@@ -0,0 +1,15 @@
+const addTodoBtn = document.querySelector(".add-todo>button");
+const todosList = document.getElementById("todos");
+
+addTodoBtn.addEventListener("click", handleClick);
+
+function handleClick() {
+  const todoInput = this.previousElementSibling;
+  const todoText = todoInput.value.trim();
+  if (todoText) {
+    const li = document.createElement("li");
+    li.innerHTML = todoText + "<span>&#128465;</span>";
+    todoInput.value = "";
+    todosList.append(li);
+  }
+}
